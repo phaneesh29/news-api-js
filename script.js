54b6f385a2334ff75a7a6f950d27c694ffcbc173
Sreephaneesha <kanugovisreephaneesha@gmail.com>
2024-08-26 22:39:12 +0530

    changed api provider

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "2d5c49a45ff345fc862808bcb4027b3e"
+const API_KEY = "c3a7a1e9415e4c3539cab051b76deb24"
 const container = document.getElementById("container")
 const mySearch = document.getElementById("my-search")
 const mySearchBtn = document.getElementById("search-btn")
@@ -6,16 +6,16 @@ const myLinks = document.querySelectorAll(".link")
 const showResults = document.getElementById("results")
 
 async function fetchData(query = "Ballari") {
-    let url = `https://newsapi.org/v2/everything?q=${query}&apiKey=${API_KEY}`
+    let url = `http://api.mediastack.com/v1/news?access_key=${API_KEY}&keywords=${query}&countries=in`
     try {
         const response = await fetch(url)
         const data = await response.json()
-        if (data.status == "ok") {
+        if (data.data.length !== 0) {
             showResults.innerHTML = `Showing Results For: ${query}`
-            bindData(data.articles)
+            bindData(data.data)
         }
         else {
-            container.innerHTML = data.message
+            container.innerHTML = "OOPS Something went wrong!"
         }
     }
     catch {
@@ -31,22 +31,18 @@ function bindData(articles) {
     else {
         container.innerHTML = ""
         articles.forEach(article => {
-            if (article.urlToImage) {
-                let artDate = new Date(article.publishedAt)
+                let artDate = new Date(article.published_at)
                 container.innerHTML += `
         <div class="card">
-                <img src="${article.urlToImage}" alt="news img">
+                <img src=${article.image ? article.image:"https://via.placeholder.com/300X250"} alt="news img">
                 <h1>${article.title}</h1>
                 <p class="date">Published at: ${artDate.toDateString()}</p>
-                <p class="source">Source: ${article.source.name}</p>
+                <p class="source">Source: ${article.source}</p>
                 <p class="desc">${article.description}</p>
-                <p class="article-content">
-                ${article.content}
-                </p>
                 <a href="${article.url}" target="_blank"><button class="read-more">Read More >></button></a>
         </div>
         `
-            }
+            
         });
     }
 }
